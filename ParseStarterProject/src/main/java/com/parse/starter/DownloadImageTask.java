6dfef61eaf85be5772cd63fe8a44a11b822d6e7b
Matt-F-Wu <haohao.wu@mail.utoronto.ca>
2016-08-15 20:37:18 -0400

    New MainActivity for app intro page display, 3 swiping pages design; Added request images save to file to minimize load time

--- ParseStarterProject/src/main/java/com/parse/starter/DownloadImageTask.java
+++ ParseStarterProject/src/main/java/com/parse/starter/DownloadImageTask.java
@@ -18,25 +18,48 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.io.InputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 /**
  * Created by HaoWu on 6/13/2016.
  */
 public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
     ImageView bmImage;
-
+    public static String reqImgPrefix = "FavReqImg";
     public DownloadImageTask(ImageView bmImage) {
         this.bmImage = bmImage;
     }
 
     protected Bitmap doInBackground(String... urls) {
+	
         String imageID = urls[0];
         Bitmap bitmap = null;
+
+	  File iFile = new File(StarterApplication.getUserFilesDir(), reqImgPrefix + imageID);
+
+	  //Try to read from file if the image exists on file
+	  if(iFile.exists()){
+	  	bitmap = BitmapFactory.decodeFile(iFile.getPath());
+		if(bitmap != null) return bitmap;
+	  }
 
         ParseQuery<ParseObject> query = ParseQuery.getQuery("ImageBox");
         try {
             ParseObject parseObject = query.get(imageID);
-            return ImageChannel.StringToBitMap(ImageChannel.getImageString(parseObject));
+            bitmap = ImageChannel.StringToBitMap(ImageChannel.getImageString(parseObject));
+		try {
+                    FileOutputStream fos = new FileOutputStream(iFile);
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 90, fos);
+                    fos.close();
+                } catch (FileNotFoundException e) {
+                    Log.d("TPIMG", "File can't be opened: " + e.getMessage());
+                } catch (IOException e) {
+                    Log.d("TPIMG", "IO Error: " + e.getMessage());
+                }
+		return bitmap;
         } catch (ParseException e) {
             e.printStackTrace();
         }
--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -1,41 +1,79 @@
-/*
- * Copyright (c) 2015-present, Parse, LLC.
- * All rights reserved.
- *
- * This source code is licensed under the BSD-style license found in the
- * LICENSE file in the root directory of this source tree. An additional grant
- * of patent rights can be found in the PATENTS file in the same directory.
- */
-package com.parse.starter;
+package com.example.haowu.testapp;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 import android.widget.ViewFlipper;
 
-import com.parse.ParseAnalytics;
 import java.io.File;
 import java.io.IOException;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
-  @Override
-  protected void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-      File file = new File(getFilesDir(), "FavouramaLicense");
+    private float x1,x2;
+    static final int MIN_DISTANCE = 150;
+    private ViewFlipper welcomeFlipper;
+    int f_index;
 
-      if(file.exists()){
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        File file = new File(getFilesDir(), "FavouramaLicense");
+
+        if(file.exists()){
           /*User already accepted license, move on*/
-          ParseAnalytics.trackAppOpenedInBackground(getIntent());
+            ParseAnalytics.trackAppOpenedInBackground(getIntent());
 
-          Intent i = new Intent(this, ACTLoginSelf.class);
-          startActivity(i);
-      }else {
-          setContentView(R.layout.activity_main);
-      }
-  }
+            Intent i = new Intent(this, ACTLoginSelf.class);
+            startActivity(i);
+        }else {
+            setContentView(R.layout.activity_main);
+            welcomeFlipper = (ViewFlipper) findViewById(R.id.welcome_flipper);
+            f_index = 0;
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        switch(event.getAction() & MotionEvent.ACTION_MASK)
+        {
+            case MotionEvent.ACTION_DOWN:
+                x1 = event.getX();
+                break;
+            case MotionEvent.ACTION_UP:
+                x2 = event.getX();
+                float deltaX = x2 - x1;
+                if (x2 - x1 >= MIN_DISTANCE)
+                {
+                    /*Swipe to right, show previous page*/
+                    welcomeFlipper.setInAnimation(AnimationUtils.loadAnimation(this, android.R.anim.slide_in_left));
+                    welcomeFlipper.setOutAnimation(AnimationUtils.loadAnimation(this, android.R.anim.slide_out_right));
+                    welcomeFlipper.showPrevious();
+                    f_index--;
+                    if(f_index < 0){
+                        f_index = 2;
+                    }
+                    highlightPoint(f_index);
+
+                } else if(x2 - x1 <= -MIN_DISTANCE){
+                    /*Swipe to left, show next page*/
+                    welcomeFlipper.setInAnimation(AnimationUtils.loadAnimation(this, R.anim.slide_in_right));
+                    welcomeFlipper.setOutAnimation(AnimationUtils.loadAnimation(this, R.anim.slide_out_left));
+                    welcomeFlipper.showNext();
+                    f_index++;
+                    f_index%=3;
+                    highlightPoint(f_index);
+                }
+                break;
+        }
+
+        return super.onTouchEvent(event);
+    }
 
     public void acceptAndProceed(View view) {
         File file = new File(getFilesDir(), "FavouramaLicense");
@@ -51,7 +89,26 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void showAgreement(View view) {
-        ViewFlipper viewFlipper = (ViewFlipper) findViewById(R.id.license_flipper);
-        viewFlipper.setDisplayedChild(1);
+        findViewById(R.id.license_agreement_container).setVisibility(View.VISIBLE);
+    }
+
+    public void highlightPoint(int index){
+        switch (index){
+            case 0:
+                ((ImageView)findViewById(R.id.findex_i1)).setImageResource(R.drawable.f_intro_point_hl);
+                ((ImageView)findViewById(R.id.findex_i2)).setImageResource(R.drawable.f_intro_point);
+                ((ImageView)findViewById(R.id.findex_i3)).setImageResource(R.drawable.f_intro_point);
+                break;
+            case 1:
+                ((ImageView)findViewById(R.id.findex_i1)).setImageResource(R.drawable.f_intro_point);
+                ((ImageView)findViewById(R.id.findex_i2)).setImageResource(R.drawable.f_intro_point_hl);
+                ((ImageView)findViewById(R.id.findex_i3)).setImageResource(R.drawable.f_intro_point);
+                break;
+            case 2:
+                ((ImageView)findViewById(R.id.findex_i1)).setImageResource(R.drawable.f_intro_point);
+                ((ImageView)findViewById(R.id.findex_i2)).setImageResource(R.drawable.f_intro_point);
+                ((ImageView)findViewById(R.id.findex_i3)).setImageResource(R.drawable.f_intro_point_hl);
+                break;
+        }
     }
 }
